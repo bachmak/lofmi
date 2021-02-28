5a38a414de5efc64879d3f93647eebf85c88cea7
Dmitry Kochetov <kochetov6969@outlook.com>
2021-02-28 12:13:16 +0700

    Separate class for the truth table

---
+++ include/lofmi/common.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <boost/dynamic_bitset.hpp>
+
+namespace Lofmi
+{
+    using Bitset = boost::dynamic_bitset<>;
+} // namespace Lofmi
---
+++ include/lofmi/truth_table.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "lofmi/common.h"
+
+#include <vector>
+
+namespace Lofmi
+{
+
+class TruthTable
+{
+public:
+    TruthTable() = default;
+    TruthTable(Bitset y);
+    TruthTable(const std::vector<bool>& bits);
+    friend std::istream& operator>>(std::istream& is, TruthTable& tt);
+    friend std::ostream& operator<<(std::ostream& os, const TruthTable& tt);
+    const std::vector<Bitset>& getX() const;
+    const Bitset& getY() const;
+
+private:
+    std::vector<Bitset> x;
+    Bitset y;
+};
+} // namespace Lofmi
