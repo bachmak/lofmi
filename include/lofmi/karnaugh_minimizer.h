2f427f38286e4e58597dabc3f4fd1c8f3478d221
Dmitry Kochetov <kochetov6969@outlook.com>
2021-03-18 20:50:35 +0700

    Karnaugh minimizer refactoring

--- include/lofmi/karnaugh_minimizer.h
+++ include/lofmi/karnaugh_minimizer.h
@@ -2,104 +2,48 @@
 
 #include "lofmi/common.h"
 #include "lofmi/utils.h"
+#include "lofmi/karnaugh_map.h"
+#include "lofmi/karnaugh_area.h"
 
 namespace Lofmi
 {
-struct Loop
+std::vector<KarnaughMapArea> findAreas(const KarnaughMap& map);
+
+namespace Minimize
 {
-    // TODO: make fields private to maintain the invariant
-    size_t top;
-    size_t bottom;
-    size_t left;
-    size_t right;
-    size_t area;
-    size_t height;
-    size_t width;
+    using Map = KarnaughMap;
+    using Area = KarnaughMapArea;
+    using Point = KarnaughMapPoint;
+    using AreasPtr = std::unique_ptr<std::vector<Area>>;
 
-    inline bool operator==(const Loop& l) const
+    // TODO: remove extra operators for debug   
+    inline std::ostream& operator<<(std::ostream& os, const AreasPtr& areas)
     {
-        return std::vector<size_t>{ top, bottom, left, right, area } ==
-            std::vector<size_t>{ l.top, l.bottom, l.left, l.right, l.area };
-    }
-
-    inline bool crossesMapXBorder() const
-    {
-        return top > bottom;
-    }
-
-    inline bool crossesMapYBorder() const
-    {
-        return left > right;
-    }
-
-    inline friend std::ostream& operator<<(std::ostream& os, const Loop& l)
-    {
-        return os << l.top << ' ' << l.bottom << ' '
-                    << l.left << ' ' << l.right << ' ' << l.area;
-    }
-
-    inline friend std::istream& operator>>(std::istream& is, Loop& l)
-    {
-        return is >> l.top >> l.bottom >> l.left >> l.right >> l.area;
-    }
-};
-
-class Minimizer
-{
-public:
-    using Loops = std::vector<Loop>;
-public:
-    Minimizer(const BoolMatrix& map);
-    Loops getMinimalForm() const;
-
-private:
-    const BoolMatrix& map;
-    const size_t y_size;
-    const size_t x_size;
-
-private:
-    struct Position
-    {
-        size_t top;
-        size_t left;
-
-        inline bool BelongsTo(const Loop& loop) const
+        for (const auto& area : *areas)
         {
-            return (
-                loop.right - loop.width + 1 <= left &&
-                loop.left + loop.width - 1 >= left &&
-                loop.bottom - loop.height + 1 <= top &&
-                loop.top + loop.height - 1 >= top
-            );
+            os << area << std::endl;
         }
 
-        inline bool IsBetweenTwoHalfsOf(const Loop& loop) const
-        {
-            bool result = false;
-
-            if (loop.crossesMapXBorder())
-            {
-                result |= top < loop.top && top > loop.bottom;
-            }
-            if (loop.crossesMapYBorder())
-            {
-                result |= left < loop.left && left > loop.right;
-            }
-
-            return result;
-        } 
-    };
+        return os;
+    }
 
     enum class Direction
     {
         Right, Down
     };
 
-private:
-    Loops findAll() const;
-    Loops findFromPosition(Position pos) const;
-    Loops findInDirection(Position pos, size_t xy_step,
-                          size_t& lim, Direction dir) const;
-    void removeOverlaps(Loops& loops) const;
-};
+    AreasPtr findAllAreas(const Map& map);
+
+    AreasPtr removeOverlappingAreas(AreasPtr areas);
+
+    AreasPtr findAreasFromPoint(const Map& map, Point point);
+
+    AreasPtr findAreasInDirectionAndUpdateLimit(
+        const Map& map, Point point, int xy_step, int& limit, Direction dir
+    );
+
+    AreasPtr moveInsert(AreasPtr src, AreasPtr dst);
+    std::pair<int, int> getXY(const Map& map);
+} // namespace Minimize
+
 } // namespace Lofmi
--- src/karnaugh_minimizer.cpp
+++ src/karnaugh_minimizer.cpp
@@ -4,63 +4,72 @@
 
 namespace Lofmi
 {
-Minimizer::Minimizer(const BoolMatrix& m) :
-    map{ m },
-    y_size{ m.size() },
-    x_size{ m.empty() ? 0 : m[0].size() } {} 
 
-Minimizer::Loops Minimizer::getMinimalForm() const
+std::vector<KarnaughMapArea> findAreas(const KarnaughMap& map)
 {
-    Loops all_loops = findAll();
-    removeOverlaps(all_loops);
-    return all_loops;
+    auto areas = Minimize::findAllAreas(map);
+    areas = Minimize::removeOverlappingAreas(std::move(areas));
+    return *areas;
 }
 
-Minimizer::Loops Minimizer::findAll() const
+namespace Minimize
 {
-    Loops result;
+// TODO: consider moving functions into class to have access to the map
 
-    for (size_t top_pos = 0; top_pos < y_size; top_pos++)
+AreasPtr findAllAreas(const Map& map)
+{
+    AreasPtr result = std::make_unique<std::vector<Area>>();
+    auto [x_size, y_size] = getXY(map);
+
+    for (int y_step = 0; y_step < y_size; y_step++)
     {
-        for (size_t left_pos = 0; left_pos < x_size; left_pos++)
+        for (int x_step = 0; x_step < x_size; x_step++)
         {
-            Loops loops = findFromPosition({ top_pos, left_pos });
-            result.insert(result.end(), loops.begin(), loops.end());
+            auto areas = findAreasFromPoint(map, Point{ x_step, y_step });
+            result = moveInsert(std::move(areas), std::move(result));
         }
     }
-
-    return result;
+         
+    return std::move(result);
 }
 
-Minimizer::Loops Minimizer::findFromPosition(Position pos) const
+AreasPtr findAreasFromPoint(const Map& map, Point point)
 {
-    Loops result;
-    size_t x_lim = x_size;
-    size_t y_lim = y_size;
+    AreasPtr result = std::make_unique<std::vector<Area>>();
 
-    for (size_t xy_step = 0; xy_step < std::min(x_lim, y_lim); xy_step++)
+    auto [x_lim, y_lim] = getXY(map);
+
+    for (int xy_step = 0; xy_step < std::min(x_lim, y_lim); xy_step++)
     {
-        Loops loops = findInDirection(pos, xy_step, x_lim, Direction::Right);
-        result.insert(result.end(), loops.begin(), loops.end());
+        result = moveInsert(
+            findAreasInDirectionAndUpdateLimit(
+                map, point, xy_step, x_lim, Direction::Right
+            ),
+            std::move(result)
+        );
 
-        loops = findInDirection(pos, xy_step, y_lim, Direction::Down);
-        result.insert(result.end(), loops.begin(), loops.end());
+        result = moveInsert(
+            findAreasInDirectionAndUpdateLimit(
+                map, point, xy_step, y_lim, Direction::Down
+            ),
+            std::move(result)
+        );
     }
 
-    return result;
+    return std::move(result);
 }
 
-Minimizer::Loops Minimizer::findInDirection(Position pos,
-                                            size_t xy_step,
-                                            size_t& lim,
-                                            Direction dir) const
+AreasPtr findAreasInDirectionAndUpdateLimit(
+    const Map& map, Point point, int xy_step, int& lim, Direction dir
+)
 {
-    Loops result;
+    AreasPtr result = std::make_unique<std::vector<Area>>();
+    auto [x_size, y_size] = getXY(map);
 
-    for (size_t step_count = 0; step_count < lim; step_count++)
+    for (int step_count = 0; step_count < lim; step_count++)
     {
-        size_t y_idx = pos.top + xy_step;
-        size_t x_idx = pos.left + xy_step;
+        int y_idx = point.y + xy_step;
+        int x_idx = point.x + xy_step;
 
         if (dir == Direction::Right)
         {
@@ -70,128 +79,110 @@ Minimizer::Loops Minimizer::findInDirection(Position pos,
         {
             y_idx += step_count;
         }
-
-        y_idx %= y_size;
+        
         x_idx %= x_size;
+        y_idx %= y_size;
 
-        if (!map[y_idx][x_idx] && step_count == 0)
+        if (!map.getMatrix()[y_idx][x_idx] && step_count == 0)
         {
             lim = 0;
         }
-        else if (!map[y_idx][x_idx] || step_count == lim - 1)
+        else if (!map.getMatrix()[y_idx][x_idx] || step_count == lim - 1)
         {
             lim = NextPowerOf2(xy_step + step_count);
 
-            Loop loop;
-            loop.area = lim * NextPowerOf2(xy_step + 1);
-            loop.top = pos.top;
-            loop.left = pos.left;
+            Area area(map);
+
+            area.setLeftTopPoint({ point.x, point.y });
 
             if (dir == Direction::Right)
             {
-                loop.bottom = (pos.top - 1 + NextPowerOf2(xy_step + 1)) % y_size;
-                loop.right = (pos.left + lim - 1) % x_size;
+                area.setHeight(NextPowerOf2(xy_step + 1));
+                area.setWidth(lim);
             }
             else
             {
-                loop.bottom = (pos.top + lim - 1) % y_size;
-                loop.right = (pos.left - 1 + NextPowerOf2(xy_step + 1)) % x_size;
+                area.setHeight(lim);
+                area.setWidth(NextPowerOf2(xy_step + 1));
             }
 
-            loop.height = (loop.bottom - loop.top + y_size) % y_size + 1;
-            loop.width = (loop.right - loop.left + x_size) % x_size + 1;
-
-            result.push_back(loop);
+            result->push_back(area);
         }
     }
     
-    return result;
+    return std::move(result);
 }
 
-void Minimizer::removeOverlaps(Loops& loops) const
+AreasPtr removeOverlappingAreas(AreasPtr areas)
 {
-    auto it = std::max_element(
-        loops.begin(), loops.end(),
-        [](const Loop& lhs, const Loop& rhs) {
-            return lhs.area < rhs.area;
-        }
-    );
+    auto it = std::max_element(areas->begin(), areas->end());
+    int max_area_size = it->getAreaSize();
 
-    int max_area = it->area;
-
-    auto non_zero_area = [](Loop& loop) { return loop.area != 0; };
-
-    for (size_t area = 1; area <= max_area; area *= 2)
+    for (int area_size = 1; area_size <= max_area_size; area_size *= 2)
     {
-        auto same_area = [area](Loop& loop) { return loop.area == area; };
+        auto has_same_area_size = std::views::filter(
+            [area_size](const Area& a)
+            { return a.getAreaSize() == area_size; }
+        );
 
-        for (Loop& ref_loop : loops |
-             std::ranges::views::filter(non_zero_area) |
-             std::ranges::views::filter(same_area))
-             // TODO: consider removing non_zero_area lambda
+        for (Area& ref_area : *areas | has_same_area_size)
         {
-            /////////////////////////////////////////////////
+            for (int i = 0; i < ref_area.getAreaSize(); i++)
+            {    
+                auto differs_from_ref_area = std::views::filter(
+                    [&ref_area](const Area& a) { return &ref_area != &a; }
+                );
 
-            // TODO: enclose in a function
+                auto non_zero_area_size = std::views::filter(
+                    [](const Area& a) { return a.getAreaSize() != 0; }
+                );
 
-            auto other_loop = [&ref_loop](Loop& loop) { return &ref_loop != &loop; };
-
-            size_t y_step = 0;
-            for ( ; y_step < ref_loop.height; y_step++)
-            {
-                size_t y_pos = (ref_loop.top + y_step) % y_size;
-
-                size_t x_step = 0;
-                for ( ; x_step < ref_loop.width; x_step++)
+                for (Area& area : *areas | 
+                    differs_from_ref_area |
+                    non_zero_area_size)
                 {
-                    size_t x_pos = (ref_loop.left + x_step) % x_size;
-
-                    bool is_sub_area = false;
-                    for (Loop& loop : loops |
-                         std::views::filter(non_zero_area) |
-                         std::views::filter(other_loop))
+                    if (area.includesPoint(ref_area[i]))
                     {
-                        Position position{ y_pos, x_pos };
-                        if (position.BelongsTo(loop))
-                        {
-                            if (position.IsBetweenTwoHalfsOf(loop))
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                is_sub_area = true;
-                                break;
-                            }
-                            
-                        }
-                    }
-
-                    if (!is_sub_area)
-                    {
+                        ref_area.reset();
                         break;
                     }
                 }
 
-                if (x_step != ref_loop.width)
+                if (ref_area.getAreaSize() == 0)
                 {
                     break;
                 }
             }
-
-            if (y_step == ref_loop.height)
-            {
-                // TODO: make reset method for Loop class
-                ref_loop.area = 0;
-            }
         }
     }
 
-    auto border = std::remove_if(
-        loops.begin(), loops.end(),
-        [](const Loop& loop) { return loop.area == 0; }
+    auto non_zero_border = std::remove_if(
+        areas->begin(), areas->end(),
+        [](const Area& a) { return a.getAreaSize() == 0; }
     );
 
-    loops.erase(border, loops.end());
+    areas->erase(non_zero_border, areas->end());
+    return std::move(areas);
 }
+
+AreasPtr moveInsert(AreasPtr src, AreasPtr dst)
+{
+    dst->insert(
+        dst->end(),
+        std::make_move_iterator(src->begin()),
+        std::make_move_iterator(src->end())
+    );
+
+    return std::move(dst);
+}
+
+std::pair<int, int> getXY(const Map& map)
+{
+    int y = static_cast<int>(map.getMatrix().size());
+    int x = y ? static_cast<int>(map.getMatrix()[0].size()) : 0;
+    return { x, y };
+}
+
+} // namespace Minimize
+
 } // namespace Lofmi
