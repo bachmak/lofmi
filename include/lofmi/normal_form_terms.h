7cc1888a8f51393a52b6c94755d4f972e10fe4a2
Dmitry Kochetov <kochetov6969@outlook.com>
2021-03-19 20:11:24 +0700

    ElemOper turned to Term hierarchy

---
+++ include/lofmi/normal_form_terms.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "lofmi/common.h"
+
+namespace Lofmi
+{
+class FullFormTerm
+{
+public:
+    friend std::ostream& operator<<(std::ostream& os, const FullFormTerm& t);
+
+protected:
+    FullFormTerm(Bitset bitset, std::string divider);
+    Bitset bitset;
+    std::string divider;
+    virtual std::string toString() const;
+};
+
+class MinFormTerm : public FullFormTerm
+{
+protected:
+    MinFormTerm(Bitset bitset, std::string div, std::vector<int> bit_numbers);
+    std::vector<int> bit_numbers;
+    std::string toString() const override;
+};
+
+class FullFormMinterm : public FullFormTerm
+{
+public:
+    FullFormMinterm(Bitset bitset);
+};
+
+class FullFormMaxterm : public FullFormTerm
+{
+public:
+    FullFormMaxterm(Bitset bitset);
+};
+
+class MinFormMinterm : public MinFormTerm
+{
+public:
+    MinFormMinterm(Bitset bitset, std::vector<int> bit_numbers);
+};
+
+class MinFormMaxterm : public MinFormTerm
+{
+public:
+    MinFormMaxterm(Bitset bitset, std::vector<int> bit_numbers);
+};    
+} // namespace Lofmi
---
+++ src/normal_form_terms.cpp
@@ -0,0 +1,66 @@
+#include "lofmi/normal_form_terms.h"
+
+namespace Lofmi
+{
+FullFormTerm::FullFormTerm(Bitset bits, std::string div) :
+    bitset{ std::move(bits) },
+    divider{ div } {}
+
+std::ostream& operator<<(std::ostream& os, const FullFormTerm& t)
+{
+    return os << t.toString();
+}
+
+std::string FullFormTerm::toString() const
+{
+    std::ostringstream os("(");
+
+    for (size_t i = 0; i < bitset.size(); i++)
+    {
+        os << (i == 0 ? "" : divider)
+            << (bitset[i] ? "" : "~")
+            << 'x' << i;
+    }
+    os << ')';
+
+    return os.str();
+}
+
+
+MinFormTerm::MinFormTerm(Bitset bits, std::string div, std::vector<int> nums) :
+    FullFormTerm(std::move(bits), div), bit_numbers{ nums }
+{
+    if (bit_numbers.size() != bits.size())
+    {
+        throw std::invalid_argument("Bitset size doesn't match labels size");
+    }
+}
+
+std::string MinFormTerm::toString() const
+{
+    std::ostringstream os("(");
+
+    for (size_t i = 0; i < bitset.size(); i++)
+    {
+        os << (i == 0 ? "" : divider)
+            << (bitset[i] ? "" : "~")
+            << 'x' << bit_numbers[i];
+    }
+    os << ')';
+
+    return os.str();
+}
+
+FullFormMinterm::FullFormMinterm(Bitset bits) : 
+    FullFormTerm(std::move(bits), " & ") {}
+
+FullFormMaxterm::FullFormMaxterm(Bitset bits) :
+    FullFormTerm(std::move(bits.flip()), " V ") {}
+
+MinFormMinterm::MinFormMinterm(Bitset bits, std::vector<int> nums) :
+    MinFormTerm(std::move(bits), " & ", std::move(bit_numbers)) {}
+
+MinFormMaxterm::MinFormMaxterm(Bitset bits, std::vector<int> nums) :
+    MinFormTerm(std::move(bits), " V ", std::move(bit_numbers)) {}
+
+} // namespace Lofmi
